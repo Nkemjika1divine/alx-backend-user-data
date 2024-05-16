87ebc8957c922ce23d8b674a728bdd593f27119a
Nkemjika1divine <cndivine@gmail.com>
2024-05-16 20:54:40 +0000

    0x00-personal_data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -18,3 +18,25 @@ def filter_datum(fields: List[str],
         replacement = f'{field}={redaction}{separator}'
         cutout_message = re.sub(pattern, replacement, message)
     return cutout_message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initializes the RedactingFormatter"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Formats a log record, redacting specified fields."""
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                record.msg, self.SEPARATOR)
+        return super().format(record)
+
+
