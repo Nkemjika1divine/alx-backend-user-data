c716363973ac0dd42a854a5f6d8eea7e252c094b
Nkemjika1divine <cndivine@gmail.com>
2024-05-21 19:40:04 +0000

    Basic Authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,7 @@ class Auth:
         """require_auth method"""
         if path is None or excluded_paths is None:
             return True
-        
+
         if path in excluded_paths:
             return False
         else:
@@ -28,13 +28,13 @@ class Auth:
                             return False
                         else:
                             return True
-    
+
     def authorization_header(self, request=None) -> str:
         """authorization_header method"""
         if not request:
             return None
         return request.headers.get('Authorization', None)
-    
+
     def current_user(self, request=None) -> TypeVar("User"):
         """current_user method"""
         return None
