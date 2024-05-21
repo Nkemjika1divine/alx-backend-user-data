bd6e0a8e769dd3201e8f03d450174535a1c53838
Nkemjika1divine <cndivine@gmail.com>
2024-05-21 19:47:38 +0000

    Basic Authentication

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,6 @@ def stats() -> str:
     return jsonify(stats)
 
 
-
 @app_views.route('/unauthorized/', strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
@@ -34,7 +33,7 @@ def unauthorized() -> str:
       - {"error": "Unauthorized"}
     """
     abort(401)
-  
+
 
 @app_views.route('/forbidden/', strict_slashes=False)
 def forbidden() -> str:
